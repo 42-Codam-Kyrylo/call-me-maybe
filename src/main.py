467fb35691cce22df18dfeec5c8c28c4cfef665e
Kyrylo Volynskyi <kvolynsk@f0r5s4.codam.nl>
2026-09-14 21:46:44 +0200

    wip: first run of model

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import argparse
+import numpy as np
 from enum import StrEnum
-
-# from llm_sdk import Small_LLM_Model
-
+from llm_sdk import Small_LLM_Model
 
 # TODO:  You must implement proper JSON error handling for input files,
 # as they may contain invalid JSON or be missing entirely
+
+
 class DefaultPath(StrEnum):
     FUNCTION_DEFINITION = "data/input/functions_definition.json"
     INPUT = "data/input/function_calling_tests.json"
@@ -29,6 +30,23 @@ def main():
 
     args = parser.parse_args()
     print("input:", args.input)
+
+    model = Small_LLM_Model()
+    prompt = "The capital of France is"
+
+    tokens: list[int] = model.encode(prompt).tolist()[0]
+    max_new_tokens = 10
+
+    for _ in range(max_new_tokens):
+        logits = model.get_logits_from_input_ids(tokens)
+        next_token = int(np.argmax(logits))
+
+        tokens.append(next_token)
+        print(model.decode([next_token]), end="", flush=True)
+
+    full_text = model.decode(tokens)
+    print()
+    print(full_text)
 
 
 if __name__ == "__main__":
