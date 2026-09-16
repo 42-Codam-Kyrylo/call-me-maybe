667bb4b4159823355149e7517da2b71fa84699c5
Kyrylo Volynskyi <kvolynsk@f1r7s1.codam.nl>
2026-09-16 22:36:32 +0200

    wip: rewrited to fsm to class and created a plan

--- src/fsm/fsm.py
+++ src/fsm/fsm.py
@@ -13,35 +13,53 @@ class STATUS(StrEnum):
     WAITING_NUMBER = "waiting_number"
 
 
-def finite_state_machine(
-    model: Small_LLM_Model,
-    vocabulary: Vocabulary,
-    functions_definitions: list[FunctionDefinition],
-    calling_tests: list[FunctionCallingTest],
-):
-    result = list[dict]
-    status = STATUS.FUNCTION_NAME
-    fd_names = [f.name for f in functions_definitions]
-    cache = Cache(model, vocabulary, fd_names)
+class FunctionCallingFSM:
+    def __init__(
+        self,
+        model: Small_LLM_Model,
+        vocabulary: Vocabulary,
+        functions_definitions: list[FunctionDefinition],
+    ):
+        self.model = model
+        self.vocabulary = vocabulary
+        self.functions_definitions = functions_definitions
+        
+        self.fd_names = [f.name for f in self.functions_definitions]
+        self.cache = Cache(self.model, self.vocabulary, self.fd_names)
+        self.status = STATUS.FUNCTION_NAME
 
-    for test in calling_tests:
-        item = {
-            "prompt": test.prompt,
-        }
+    def run_tests(self, calling_tests: list[FunctionCallingTest]) -> list[dict]:
+        result = []
 
-        prompt = generate_prompt(functions_definitions, test.prompt)
+        for test in calling_tests:
+            item = {
+                "prompt": test.prompt,
+            }
 
-        match status:
-            case STATUS.FUNCTION_NAME:
-                name = get_function_name()
-                item.name = name
-            case STATUS.FUNCTION_PARAMETERS:
-                pass
-            case STATUS.WAITING_NUMBER:
-                pass
+            prompt = generate_prompt(self.functions_definitions, test.prompt)
 
-        result.append(item)
+            match self.status:
+                case STATUS.FUNCTION_NAME:
+                    name = self._get_function_name(prompt)
+                    item["name"] = name
+                case STATUS.FUNCTION_PARAMETERS:
+                    pass
+                case STATUS.WAITING_NUMBER:
+                    pass
 
+            result.append(item)
+            
+        return result
 
-def get_function_name() -> str:
-    pass
+    def _get_function_name(self, prompt: str) -> str:
+        # TODO: 
+        # result = []
+        # while len(autocomplite(model.decode(result))) != 1 // going through all functions and returns all functions that starts from model.decode(result)
+        # encode prompt + inject "name": "fn_ (add check if all fd starts with fn_)
+        # logits = self.model.get_logits_from_input_ids(promt_tokens)
+        # through np allow only tokens in cache.valid_fd_ids
+        # choose - next_token =  int(np.argmax(logits))
+        # prompt_tokens.append(next_token)
+        # result.append(next_token)
+        #
+        pass
